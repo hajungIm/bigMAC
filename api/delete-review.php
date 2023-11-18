aa044979983e266ea99dfe95c477a0cbe885d952
ji-somnie <ie8907@ewhain.net>
2023-11-18 17:44:51 +0900

    update delete-review.php

--- api/delete-review.php
+++ api/delete-review.php
@@ -3,20 +3,42 @@
 $mysqli = new mysqli("localhost", "team04", "team04", "team04");
 
 if ($mysqli->connect_error) {
-  die("Connection failed: " . $mysqli->connect_error);
+    die("Connection failed: " . $mysqli->connect_error);
 }
 
 $reviewId = $_POST['reviewId'];
 
-$stmt = $mysqli->prepare("DELETE FROM review WHERE review_id = ?");
+//현재 리뷰의 레스토랑 ID
+$stmt = $mysqli->prepare("SELECT restaurant_id FROM review WHERE review_id = ?");
 $stmt->bind_param("i", $reviewId);
 
 if ($stmt->execute()) {
-  echo "Review deleted successfully";
+    $stmt->bind_result($restaurantId);
+    $stmt->fetch();
+    $stmt->close();
+
+    // 리뷰 삭제
+    $stmt = $mysqli->prepare("DELETE FROM review WHERE review_id = ?");
+    $stmt->bind_param("i", $reviewId);
+
+    if ($stmt->execute()) {
+        // 리뷰가 삭제 시 해당 레스토랑의 review_count - 1
+        $stmt = $mysqli->prepare("UPDATE restaurant SET review_count = review_count - 1 WHERE restaurant_id = ?");
+        $stmt->bind_param("i", $restaurantId);
+        
+        if ($stmt->execute()) {
+            echo "Review deleted successfully";
+        } else {
+            echo "Error updating restaurant review count: " . $mysqli->error;
+        }
+
+        $stmt->close();
+    } else {
+        echo "Error deleting review: " . $mysqli->error;
+    }
 } else {
-  echo "Error deleting review: " . $mysqli->error;
+    echo "Error retrieving restaurant ID: " . $mysqli->error;
 }
 
-$stmt->close();
 $mysqli->close();
- ?>
+?>
