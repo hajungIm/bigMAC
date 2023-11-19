152848ed6c054f5c291648554cb4d267ddf349b9
ji-somnie <ie8907@ewhain.net>
2023-11-19 20:32:09 +0900

    add transaction

--- api/delete-review.php
+++ api/delete-review.php
@@ -8,36 +8,42 @@ if ($mysqli->connect_error) {
 
 $reviewId = $_POST['reviewId'];
 
-//현재 리뷰의 레스토랑 ID
-$stmt = $mysqli->prepare("SELECT restaurant_id FROM review WHERE review_id = ?");
-$stmt->bind_param("i", $reviewId);
+$mysqli->begin_transaction();
 
-if ($stmt->execute()) {
-    $stmt->bind_result($restaurantId);
-    $stmt->fetch();
-    $stmt->close();
-
-    // 리뷰 삭제
-    $stmt = $mysqli->prepare("DELETE FROM review WHERE review_id = ?");
+try {
+    $stmt = $mysqli->prepare("SELECT restaurant_id FROM review WHERE review_id = ?");
     $stmt->bind_param("i", $reviewId);
 
     if ($stmt->execute()) {
-        // 리뷰가 삭제 시 해당 레스토랑의 review_count - 1
-        $stmt = $mysqli->prepare("UPDATE restaurant SET review_count = review_count - 1 WHERE restaurant_id = ?");
-        $stmt->bind_param("i", $restaurantId);
-        
-        if ($stmt->execute()) {
-            echo "Review deleted successfully";
-        } else {
-            echo "Error updating restaurant review count: " . $mysqli->error;
-        }
-
+        $stmt->bind_result($restaurantId);
+        $stmt->fetch();
         $stmt->close();
+
+        // 리뷰 삭제
+        $stmt = $mysqli->prepare("DELETE FROM review WHERE review_id = ?");
+        $stmt->bind_param("i", $reviewId);
+
+        if ($stmt->execute()) {
+            $stmt = $mysqli->prepare("UPDATE restaurant SET review_count = review_count - 1 WHERE restaurant_id = ?");
+            $stmt->bind_param("i", $restaurantId);
+
+            if ($stmt->execute()) {
+                $mysqli->commit();
+                echo "Review deleted successfully";
+            } else {
+                throw new Exception("Error updating restaurant review count: " . $mysqli->error);
+            }
+
+            $stmt->close();
+        } else {
+            throw new Exception("Error deleting review: " . $mysqli->error);
+        }
     } else {
-        echo "Error deleting review: " . $mysqli->error;
+        throw new Exception("Error retrieving restaurant ID: " . $mysqli->error);
     }
-} else {
-    echo "Error retrieving restaurant ID: " . $mysqli->error;
+} catch (Exception $e) {
+    $mysqli->rollback();
+    echo $e->getMessage();
 }
 
 $mysqli->close();
